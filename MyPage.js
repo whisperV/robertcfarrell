37c7c4837ad7cf28f2a266efc5dc516990614122
whisperV <whisper.n00f@gmail.com>
2017-07-13 21:32:33 -0700

    Expand/collapse toggle
    Add expand/collapse toggle for the job descriptions.

--- MyPage.js
+++ MyPage.js
@@ -1,9 +1,29 @@
-$("#example-two").on("click", function() {
-  var el = $(this);
-  if (el.text() == el.data("text-swap")) {
-    el.text(el.data("text-original"));
-  } else {
-    el.data("text-original", el.text());
-    el.text(el.data("text-swap"));
-  }
-});
+
+// Toggle Job duties description
+
+var expander = document.getElementsByTagName("button");
+var isOpen = true;
+
+expander[1].addEventListener("click", function toggleButton(){
+	if (isOpen){
+			expander[1].classList.add('closed');
+			expander[2].classList.remove('closed');
+	} else {
+			expander[2].classList.add('closed');
+			expander[1].classList.remove('closed');
+			}
+		isOpen = !isOpen;
+	})	
+
+expander[2].addEventListener("click", function toggleButton2(){
+	if (isOpen){
+			expander[1].classList.add('closed');
+			expander[2].classList.remove('closed');
+	} else {
+			expander[2].classList.add('closed');
+			expander[1].classList.remove('closed');
+			}
+		isOpen = !isOpen;
+	})	
+
+
