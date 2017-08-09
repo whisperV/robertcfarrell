c0433ba322bea1beea73b0db0ebc0b177f3348aa
whisperV <whisper.n00f@gmail.com>
2017-08-09 13:05:33 -0700

    Bug fix and minor style changes
    Fixed the Open All button on the Resume page, added some style distinction for the dev blog

--- resume.js
+++ resume.js
@@ -1,53 +1,19 @@
 
 // Toggle Job duties description
 
-var expander = document.getElementsByTagName("button");
-var isOpen = true;
-
-
-//RESUME
-
-// expander[1].addEventListener("click", function toggleButton(){
-// 	if (isOpen){
-// 			expander[1].classList.add('closed');
-// 			expander[2].classList.remove('closed');
-// 	} else {
-// 			expander[2].classList.add('closed');
-// 			expander[1].classList.remove('closed');
-// 			}
-// 		isOpen = !isOpen;
-// 	})	
-
-// expander[2].addEventListener("click", function toggleButton2(){
-// 	if (isOpen){
-// 			expander[1].classList.add('closed');
-// 			expander[2].classList.remove('closed');
-// 	} else {
-// 			expander[2].classList.add('closed');
-// 			expander[1].classList.remove('closed');
-// 			}
-// 		isOpen = !isOpen;
-// 	})	
 
 
 $("#openAll").on("click", function(){
 	$(this).toggleClass("collapse");
 	$("#closeAll").toggleClass("collapse");
-	$(".workDesc").addClass("show");
 	$(".btn-custom").removeClass("collapsed");
-	
+	if ($(".workDesc").not(".in")){
+		$(".workDesc").addClass("in");
+		$(".workDesc").css("height", "auto");
+	}
 });
 
-
-// $("#openAll").on("click", function(){
-// 	$(".btn-custom").attr({
-// 		"aria-expanded": true
-// 		"class" : "btn btn-custom"})
-// 	$(".workDesc").attr({
-		
-// 	})
-// });
-
+	
 $("#closeAll").on("click", function(){
 	$(this).toggleClass("collapse");
 	$("#openAll").toggleClass("collapse");
