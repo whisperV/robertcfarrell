64cdbbcc8c9f143a96adf6a77c4428d4b9709805
whisperV <whisper.n00f@gmail.com>
2018-07-23 13:17:39 -0700

    Redirect to secure page
    Include code to redirect to https://

--- landing.js
+++ landing.js
@@ -4,3 +4,8 @@
 // $("h1").click(function(){
 // 	alert("button clicked");
 // 	});
+
+var sec = window.location.href+'';
+if (sec.indexOf('http://')==0){
+    window.location.href = sec.replace('http://','https://');
+}
