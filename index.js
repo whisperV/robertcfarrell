2f28527e063366126bae4e71805e9e238db452fa
whisperv <whisper.n00f@gmail.com>
2021-01-10 09:49:48 -0800

    styling overhaul/refactor

--- index.js
+++ index.js
@@ -1,35 +1,78 @@
-// CHANGE TO CONSTS
 
-// CREATE NAMESPACE?
 
-const   mDiv = document.getElementById("mainDiv"),
-        secDiv = document.getElementById("secondDiv"),
-        dLoader = document.getElementById("loadItD"),
-        bLoader = document.getElementById("loadItB"),
-        devButText = document.getElementById("devLoad"),
-        blogButText = document.getElementById('blogLoad');
+const   aboutButton = document.getElementById('about'),
+        blogButton = document.getElementById('blog'),
+        readButton = document.getElementById('articles'),
+        allButtons = document.getElementsByClassName('navbuttons');
 
-function blogIt(){  
-        devButText.className="";
-        dLoader.className="hideMe";
-        blogButText.className="hideMe";
-        bLoader.className=""; 
+const blogIt = () => {  
+    window.location = 'https://blogbert.herokuapp.com/'  
+    blogButton.innerText = 'Loading';
+    blogButton.className += " spinner-grow";
+    setTimeout(()=> {
+        blogButton.innerText = 'Blog';
+        blogButton.className = "navbuttons navimation";
+    }, 5000)                      
 }
 
-function aboutIt(){
-        mDiv.className='hideMe';
-        secDiv.className='';
-
-        devButText.className="";
-        dLoader.className="hideMe";
-        blogButText.className="";
-        bLoader.className="hideMe";    
+const aboutIt = () => {
+    window.location.pathname = '/about.html'  
+    aboutButton.innerText = 'Loading';
+    aboutButton.className += " spinner-grow";
+    setTimeout(()=> {
+        aboutButton.innerText = 'Blog';
+        aboutButton.className = "navbuttons navimation";
+    }, 5000)           
 }
 
-function devIt(){
-    dLoader.className="";
-    devButText.className="hideMe";
-    blogButText.className="";
-    bLoader.className="hideMe";
+const devIt = (event) => {
+    window.location.pathname = '/apps.html';
+    // var cX = event.clientX;
+    // var sX = event.screenX;
+    // var cY = event.clientY;
+    // var sY = event.screenY;
+    // var coords1 = "client - X: " + cX + ", Y coords: " + cY;
+    // var coords2 = "screen - X: " + sX + ", Y coords: " + sY;
+    // document.getElementById("cursorloc").innerHTML = coords1 + "<br>" + coords2;
 }
 
+const readIt = () => {
+    readButton.innerText = 'Loading';
+    readButton.className += " spinner-grow";    
+    setTimeout(()=> {
+        readButton.innerText = 'Read';
+        readButton.className = "navbuttons navimation";
+    }, 5000)             
+}
+
+const simpleEstimate = () => {
+    window.location='Src/TaxApps/TaxCalc/SimpleTaxEstimator.html'
+    
+}
+const iraCalc = () => {
+    window.location='Src/TaxApps/RetireContribs/RetirementContributions.html'
+}
+
+const buttonResponse = () => {     
+    // for(button of allButtons){
+    //     if(button.classList.contains('wavy')){
+    //         button.classList.remove('wavy')
+    //         button.classList.add('gravy')       
+    //     } else {
+    //         button.classList.add('wavy')
+    //         button.classList.remove('gravy')
+    //     }
+        
+    // }      
+}
+
+$(document).ready(function() {
+        $('.nav-container').ripples({
+            resolution: 512,
+            dropRadius: 10,
+            perturbance: 0.02,
+        });
+});
+
+// Add a 'drop' to the ripples
+// $('body').ripples("drop", x, y, radius, strength)
